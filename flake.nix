f0b6534207de39b2294d3dd7578c3705395d8fd8
hugosenari <hugosenari@gmail.com>
2022-03-18 17:09:32 -0300

    chore: re index cal

--- flake.nix
+++ flake.nix
@@ -7,6 +7,22 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."cal-master".dir   = "master";
+  inputs."cal-master".owner = "nim-nix-pkgs";
+  inputs."cal-master".ref   = "master";
+  inputs."cal-master".repo  = "cal";
+  inputs."cal-master".type  = "github";
+  inputs."cal-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."cal-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."cal-v0_2_0".dir   = "v0_2_0";
+  inputs."cal-v0_2_0".owner = "nim-nix-pkgs";
+  inputs."cal-v0_2_0".ref   = "master";
+  inputs."cal-v0_2_0".repo  = "cal";
+  inputs."cal-v0_2_0".type  = "github";
+  inputs."cal-v0_2_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."cal-v0_2_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
